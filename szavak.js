39f33b12e1a294a723093bdf9b017106412cedb8
s2h3s2t3r <szabo.horvath.eszter@gmail.com>
2024-10-08 14:28:56 +0200

    Add files via upload

---
+++ szavak.js
@@ -0,0 +1,38 @@
+const wordList = [
+    {
+        word: "bécs",
+        hint: "Itt született."
+    },
+    {
+        word: "napóleon",
+        hint: "Ellene harcolt 1814-ben."
+    },
+    {
+        word: "anglia",
+        hint: "Ide is elutazott."
+    },
+    {
+        word: "mta",
+        hint: "Jövedelmét ajánlotta fel, hogy létrehozzák."
+    },
+    {
+        word: "hitel",
+        hint: "Első jelentős műve."
+    },
+    {
+        word: "kossuth",
+        hint: "Vele sokszor nem értettek egyet."
+    },
+    {
+        word: "lánchíd",
+        hint: "Az első állandó dunai híd Pest és Buda között."
+    },
+    {
+        word: "duna",
+        hint: "Ezt is szabályozta."
+    },
+    {
+        word: "döbling",
+        hint: "Itt hunyt el."
+    },
+]
